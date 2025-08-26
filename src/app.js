a00e92501dd7333c94b5daee746b976a05136cd6
Melyssa Nimeth <147218092+nimethv@users.noreply.github.com>
2025-08-26 18:34:31 -0300

    API Update
    CRUD and Modular Architecture (Adapted MVC)

--- src/app.js
+++ src/app.js
@@ -1,53 +1,68 @@
 import express from "express";
+import conectaNaDatabase from "./config/dbConnect.js";
+// import livro from "./models/Livro.js";
+import routes from "./routes/livrosRoutes.js"
+
+const conexao = await conectaNaDatabase();
+
+conexao.on("error", (erro) => {
+    console.error("erro de conexão", erro);
+});
+
+conexao.once("open", () => {
+    console.log("conexão com o banco feita com sucesso");
+});
 
 const app = express();
-app.use(express.json());
+// app.use(express.json());
+routes(app);
 
-const livros = [
-    {
-        id: 1,
-        titulo: "battle royale"
-    },
-    {
-        id: 2,
-        titulo: "blame!"
-    }
-]
+// const livros = [
+//     {
+//         id: 1,
+//         titulo: "battle royale"
+//     },
+//     {
+//         id: 2,
+//         titulo: "blame!"
+//     }
+// ]
 
-function buscaLivro(id){
-    return livros.findIndex(livro => {
-        return livro.id === Number(id);
-    })
-}
+// function buscaLivro(id){
+//     return livros.findIndex(livro => {
+//         return livro.id === Number(id);
+//     })
+// }
 
-app.get("/", (req, res) => {
-    res.status(200).send("Curso de Node.js");
-});
+// app.get("/", (req, res) => {
+//     res.status(200).send("Curso de Node.js");
+// });
 
-app.get("/livros", (req, res) => {
-    res.status(200).json(livros);
-});
+// app.get("/livros", async (req, res) => {
+//     const listaLivros = await livro.find({});
+//     res.status(200).json(listaLivros);
+// });
 
-app.get("/livros/:id", (req, res) => {
-    const index = buscaLivro(req.params.id);
-    res.status(200).json(livros[index]);
-});
+// app.get("/livros/:id", (req, res) => {
+//     const index = buscaLivro(req.params.id);c
+//     res.status(200).json(livros[index]);
+// });
 
-app.post("/livros", (req, res) => {
-    livros.push(req.body);
-    res.status((201).send("livro cadastrado com sucesso"))
-});
+// app.post("/livros", (req, res) => {
+//     livros.push(req.body);
+//     res.status((201).send("livro cadastrado com sucesso"))
+// });
 
-app.put("/livros/:id", (req, res) => {
-    const index = buscaLivro(req.params.id);
-    livros[index].titulo = req.body.titulo; 
-    res.status(200).json(livros);
-});
+// app.put("/livros/:id", (req, res) => {
+//     const index = buscaLivro(req.params.id);
+//     livros[index].titulo = req.body.titulo; 
+//     res.status(200).json(livros);
+// });
 
-app.delete("/livros/:id", (req, res) => {
-    const index = buscaLivro(req.params.id);
-    livros.splice(index, 1);
-    res.status(204).send("livro removido com sucesso");
-});
+// app.delete("/livros/:id", (req, res) => {
+//     const index = buscaLivro(req.params.id);
+//     livros.splice(index, 1);
+//     res.status(204).send("livro removido com sucesso");
+// });
 
 export default app;
